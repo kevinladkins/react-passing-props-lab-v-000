5a473bf2053c8f5d97110ba473afe0fb02b6c711
github username <kevinladkins@gmail.com>
2017-06-12 21:37:15 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,44 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fruit: [],
+      filters: [],
+      currentFilter: null,
+    }
+  }
 
-export default App;
+  componentWillMount() {
+    this.fetchFilters();
+    this.fetchFruit();
+  }
+
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
+
+  fetchFruit = () => {
+    fetch('/api/fruit')
+      .then(response => response.json())
+      .then(items => this.setState({ fruit: items }));
+  }
+
+
+  handleFilterChange = event => {
+    console.log('new filter: ', event.target.value);
+    this.setState({
+      currentFilter: event.target.value 
+    });
+  }
+
+  render() {
+    return(
+      <FruitBasket fruit={this.state.fruit} filters={this.state.filters} updateFilterCallback={this.handleFilterChange} currentFilter={this.state.currentFilter}/>
+    );
+  }
+}
--- src/components/FilteredFruitList.js
+++ src/components/FilteredFruitList.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilteredFruitList extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      items: []
-    };
-  }
 
-  componentWillMount() {
-    fetch('/api/fruit')
-      .then(response => response.json())
-      .then(items => this.setState({ items }));
-  }
-
-  render() {
-    const list = !this.props.filter ? this.state.items : this.state.items.filter(i => i.fruit_type === this.props.filter);
-
-    return (
+const FilteredFruitList = ({filter, fruit}) => {
+  const list = !filter ? fruit : fruit.filter(i => i.fruit_type === filter);
+  return (
       <ul className="fruit-list">
-        {list.map((item, index) => <li key={index}>{item.char}</li>)}
+        {list.map((list, index) => <li key={index}>{list.char}</li>)}
       </ul>
-    );
-  }
+    )
 }
+
+FilteredFruitList.defaultProps = {
+  fruit: [],
+  filter: null
+}
+
 
 export default FilteredFruitList;
